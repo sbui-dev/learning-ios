38d88f6c251e33ab889cd31bcea0883dcda19d94
engineer-sbui <sbui.513@gmail.com>
2019-03-15 02:41:23 -0700

    I Am Rich proj

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Steven Bui on 3/15/19.
+//  Copyright © 2019 Steven Bui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
