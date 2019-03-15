80598ebc85dae12c342256a8463e5e6cde2d8e04
System Administrator <root@Stevens-MacBook-Air.local>
2019-03-14 22:37:06 -0700

    HelloWorld proj

---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Steven Bui on 3/7/19.
+//  Copyright © 2019 Steven Bui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
