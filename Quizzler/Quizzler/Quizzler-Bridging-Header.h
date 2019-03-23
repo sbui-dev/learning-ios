164db70adf072c97fd62bc9a8a35ef1595b8b3b8
engineer-sbui <sbui.513@gmail.com>
2019-03-22 23:50:00 -0700

    Quizzler proj

---
+++ Quizzler/Quizzler/Quizzler-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Quizzler-Bridging-Header.h
+//  Quizzler
+//
+//  Created by Steven Bui on 3/22/19.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+#ifndef Quizzler_Bridging_Header_h
+#define Quizzler_Bridging_Header_h
+
+#import "ProgressHUD.h"
+
+#endif /* Quizzler_Bridging_Header_h */
