49cca8e447ef07289684386fc239248e2183417c
engineer-sbui <sbui.513@gmail.com>
2019-03-26 01:17:38 -0700

    BitcoinTicker proj

---
+++ BitcoinTicker/BitcoinTicker/ViewController.swift
@@ -0,0 +1,79 @@
+//
+//  ViewController.swift
+//  BitcoinTicker
+//
+//  Created by Angela Yu on 23/01/2016.
+//  Copyright © 2016 London App Brewery. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    let baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC"
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    let currencySymbolArray = ["$", "R$", "$", "¥", "€", "£", "$", "Rp", "₪", "₹", "¥", "$", "kr", "$", "zł", "lei", "₽", "kr", "$", "$", "R"]
+    var finalURL = ""
+    var currencySymbol = ""
+
+    //Pre-setup IBOutlets
+    @IBOutlet weak var bitcoinPriceLabel: UILabel!
+    @IBOutlet weak var currencyPicker: UIPickerView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        currencyPicker.delegate = self
+        currencyPicker.dataSource = self
+        currencySymbol = currencySymbolArray[0]
+        finalURL = baseURL + currencyArray[0]
+        getBitcoinData(url : finalURL)
+    }
+
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return currencyArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return currencyArray[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        finalURL = baseURL + currencyArray[row]
+        currencySymbol = currencySymbolArray[row]
+        getBitcoinData(url : finalURL)
+    }
+    
+    
+    //MARK: - Networking
+    /***************************************************************/
+    
+    func getBitcoinData(url: String) {
+        Alamofire.request(url, method: .get).responseJSON {
+            response in
+                if response.result.isSuccess {
+                    let priceJSON : JSON = JSON(response.result.value!)
+
+                    self.updateBitconPrice(json: priceJSON)
+
+                } else {
+                    print("Error: \(String(describing: response.result.error))")
+                    self.bitcoinPriceLabel.text = "Connection Issues"
+                }
+            }
+    }
+  
+    //MARK: - JSON Parsing
+    /***************************************************************/
+    func updateBitconPrice(json : JSON) {
+        
+        if let tempResult = json["last"].double {
+            bitcoinPriceLabel.text = "\(currencySymbol)\(tempResult)"
+        }
+    }
+}
+
