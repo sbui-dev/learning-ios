bdd29f231d316926cce868e881894b720b8e8f69
engineer-sbui <sbui.513@gmail.com>
2019-03-27 22:59:17 -0700

    add persistent storage using user defaults method

--- Todoey/Todoey/ToDoListViewController.swift
+++ Todoey/Todoey/ToDoListViewController.swift
@@ -11,10 +11,15 @@ import UIKit
 class ToDoListViewController: UITableViewController {
     
     var itemArray = ["Find Mike", "Buy Eggos", "Destory Demogorgon"]
+    
+    let defaults = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        if let items = UserDefaults.standard.array(forKey: "ToDoListArray") as? [String] {
+            itemArray = items
+        }
     }
 
     //MARK: Tableview Datasource Methods
@@ -55,6 +60,7 @@ class ToDoListViewController: UITableViewController {
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             //Add Item button action
             self.itemArray.append(textField.text!)
+            self.defaults.set(self.itemArray, forKey: "ToDoListArray")
             self.tableView.reloadData()
         }
         
