91f42003c5b27cc4e47a0ffb133a15ee96fded18
engineer-sbui <sbui.513@gmail.com>
2019-04-08 14:58:41 -0700

    Change navi title accordingly if it's hotdog or not hotdog

--- SeeFood/SeeFood/ViewController.swift
+++ SeeFood/SeeFood/ViewController.swift
@@ -49,7 +49,15 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             guard let results = request.results as? [VNClassificationObservation] else {
                 fatalError("Failed to process image")
             }
-            print(results)
+            
+            if let firstResult = results.first {
+                if firstResult.identifier.contains("hotdog") {
+                    self.navigationItem.title = "Hotdog!"
+                }
+                else {
+                    self.navigationItem.title = "Not Hotdog!"
+                }
+            }
         }
         
         let handler = VNImageRequestHandler(ciImage: image)
