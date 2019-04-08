89a15ea6b6b527627393f14cc76efa8747e2f4ba
engineer-sbui <sbui.513@gmail.com>
2019-04-08 02:54:53 -0700

    Initial empty SeeFood project

---
+++ SeeFood/SeeFood/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by Steven Bui on 4/8/19.
+//  Copyright © 2019 Steven Bui. All rights reserved.
+//
+
+import UIKit
+import CoreML
+import Vision
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
