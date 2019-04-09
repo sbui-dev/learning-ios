7c14fefa38564139f7450255d4b17b3c0ff10f6f
engineer-sbui <sbui.513@gmail.com>
2019-04-08 20:09:09 -0700

    Add logic to query IBM watson to recognize if image is a hotdog or not

--- SeeFood-IBM/SeeFood_IBM/ViewController.swift
+++ SeeFood-IBM/SeeFood_IBM/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import VisualRecognition
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
@@ -14,6 +15,10 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     @IBOutlet weak var imageView: UIImageView!
     
     let imagePicker = UIImagePickerController()
+    let apiKey = ""
+    let version = "2019-04-08"
+    
+    var classificationResults : [String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,6 +31,45 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
             imageView.image = image
             imagePicker.dismiss(animated: true, completion: nil)
+            
+            let visualRecognition = VisualRecognition(version: version, apiKey: apiKey)
+            
+            guard let imageData = image.jpegData(compressionQuality: 0.01) else {
+                fatalError("Compression failed")
+            }
+            
+            guard let smallerImage = UIImage(data: imageData) else {
+                fatalError("Image data conversion failed")
+            }
+            
+            visualRecognition.classify(image: smallerImage) { (response, error) in
+                if let err = error {
+                    print(err)
+                }
+                if let classfiedImages = response?.result {
+                    
+                    self.classificationResults = []
+                    
+                    let classes = classfiedImages.images.first!.classifiers.first!.classes
+                    
+                    for index in 0..<classes.count {
+                        self.classificationResults.append(classes[index].className)
+                    }
+                    
+                    print(self.classificationResults)
+                    
+                    if self.classificationResults.contains("hotdog") {
+                        DispatchQueue.main.async {
+                            self.navigationItem.title = "Hotdog!"
+                        }
+                    }
+                    else {
+                        DispatchQueue.main.async {
+                            self.navigationItem.title = "Not Hotdog!"
+                        }
+                    }
+                }
+            }
         }
         else {
             print("Error picking image")
