2004ea4f018d963922d8df529001df42a0446a9e
engineer-sbui <sbui.513@gmail.com>
2019-04-11 00:43:54 -0700

    Add SDWebImage and use Wikipedia's image

--- WhatFlower/WhatFlower/ViewController.swift
+++ WhatFlower/WhatFlower/ViewController.swift
@@ -11,6 +11,7 @@ import CoreML
 import Vision
 import Alamofire
 import SwiftyJSON
+import SDWebImage
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
@@ -36,10 +37,6 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             }
             
             detect(image: ciImage)
-            
-            imageView.image = userImage
-            
-            
         }
         
         imagePicker.dismiss(animated: true, completion: nil)
@@ -81,12 +78,13 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         let parameters : [String:String] = [
             "format" : "json",
             "action" : "query",
-            "prop" : "extracts",
+            "prop" : "extracts|pageimages",
             "exintro" : "",
             "explaintext" : "",
             "titles" : flowerName,
             "indexpageids" : "",
             "redirects" : "1",
+            "pithumbsize" : "500"
         ]
         
         Alamofire.request(wikipediaURl, method : .get, parameters : parameters).responseJSON {
@@ -97,7 +95,10 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
                 
                 let pageid = flowerJSON["query"]["pageids"][0].stringValue
                 let flowerDesc = flowerJSON["query"]["pages"][pageid]["extract"].stringValue
+                let flowerImageURL = flowerJSON["query"]["pages"][pageid]["thumbnail"]["source"].stringValue
 
+                self.imageView.sd_setImage(with: URL(string : flowerImageURL))
+                
                 self.flowerLabel.text = flowerDesc
             }
         }
