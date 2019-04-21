053998cc95a6a1d1d7addda258fb7f0d5171e222
engineer-sbui <sbui.513@gmail.com>
2019-04-20 22:18:00 -0700

    initial skeleton AR project

---
+++ ARDicee/ARDicee/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  ARDicee
+//
+//  Created by Steven Bui on 4/20/19.
+//  Copyright © 2019 Steven Bui. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // Create a new scene
+        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        
+        // Set the scene to the view
+        sceneView.scene = scene
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+}
