7fe0a760cbfe43543329a6dc4f5d137ccf29bbbb
engineer-sbui <sbui.513@gmail.com>
2019-04-21 02:44:03 -0700

    initial skeleton project

---
+++ AR-Ruler/AR-Ruler/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  AR-Ruler
+//
+//  Created by Steven Bui on 4/21/19.
+//  Copyright © 2019 Steven Bui. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
+        
+
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+}
