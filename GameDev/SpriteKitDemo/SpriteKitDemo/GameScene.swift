cc90007d315677829f675f7664055e8e5aebef7a
engineer-sbui <sbui.513@gmail.com>
2019-04-24 01:02:52 -0700

    initial empty project for spritekit demo

---
+++ GameDev/SpriteKitDemo/SpriteKitDemo/GameScene.swift
@@ -0,0 +1,16 @@
+//
+//  GameScene.swift
+//  SpriteKitDemo
+//
+//  Created by Steven Bui on 4/24/19.
+//  Copyright © 2019 Steven Bui. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+}
---
+++ GameDev/SpriteKitDemo/SpriteKitDemo/GameViewController.swift
@@ -0,0 +1,34 @@
+//
+//  GameViewController.swift
+//  SpriteKitDemo
+//
+//  Created by Steven Bui on 4/24/19.
+//  Copyright © 2019 Steven Bui. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            
+            let scene = GameScene(size: view.bounds.size)
+            
+            // Set the scale mode to scale to fit the window
+            scene.scaleMode = .aspectFill
+            
+            // Present the scene
+            view.presentScene(scene)
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+            view.showsPhysics = true
+        }
+    }
+}
