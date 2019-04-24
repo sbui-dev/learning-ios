2c8da02ccd9945c38e85c3807248a0c7b3cd722d
engineer-sbui <sbui.513@gmail.com>
2019-04-23 17:10:22 -0700

    initial commit for simon says

---
+++ GameDev/SimonSays/SimonSays/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimonSays
+//
+//  Created by Steven Bui on 4/23/19.
+//  Copyright © 2019 Steven Bui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
