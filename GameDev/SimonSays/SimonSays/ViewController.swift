b602def6ad86926e83e00c46318375a8b9726587
engineer-sbui <sbui.513@gmail.com>
2019-04-23 23:42:24 -0700

    add logic to create random sequence to press and create flashing button

--- GameDev/SimonSays/SimonSays/ViewController.swift
+++ GameDev/SimonSays/SimonSays/ViewController.swift
@@ -16,6 +16,13 @@ class ViewController: UIViewController {
     @IBOutlet var playerLabels: [UILabel]!
     @IBOutlet var scoreLabels: [UILabel]!
     
+    var currentPlayer = 0
+    var scores = [0,0]
+    
+    var sequenceIndex = 0
+    var colorSequence = [Int]()
+    var colorsToTap = [Int]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         colorButtons = colorButtons.sorted() {
@@ -32,10 +39,45 @@ class ViewController: UIViewController {
     }
     
     func createNewGame() {
+        colorSequence.removeAll()
+        
         actionButton.setTitle("Start Game", for: .normal)
+        actionButton.isEnabled = true
         
         for button in colorButtons {
             button.alpha = 0.5
+            button.isEnabled = false
+        }
+        
+    }
+    
+    func addNewColor() {
+        colorSequence.append(Int(arc4random_uniform(UInt32(4))))
+    }
+    
+    func playSequence() {
+        if sequenceIndex < colorSequence.count {
+            flash(button: colorButtons[colorSequence[sequenceIndex]])
+            sequenceIndex += 1
+        }
+        else {
+            colorsToTap = colorSequence
+            view.isUserInteractionEnabled = true
+            actionButton.setTitle("Tap the Circles", for: .normal)
+            for button in colorButtons {
+                button.isEnabled = true
+            }
+        }
+    }
+    
+    func flash(button: CircularButton) {
+        
+        // create a flash by changing alpha of button
+        UIView.animate(withDuration: 0.5, animations: {
+            button.alpha = 1.0
+            button.alpha = 0.5
+        }) { (bool) in
+            self.playSequence()
         }
     }
 
@@ -44,7 +86,21 @@ class ViewController: UIViewController {
     }
     
     @IBAction func actionButtonHandler(_ sender: UIButton) {
-        print("Action Button")
+        // reset sequence
+        sequenceIndex = 0
+        
+        actionButton.setTitle("Memorize", for: .normal)
+        actionButton.isEnabled = false
+        
+        // disable all UI
+        view.isUserInteractionEnabled = false
+        
+        addNewColor()
+        
+        // delay by 1 second
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(1)) {
+            self.playSequence()
+        }
     }
     
 }
