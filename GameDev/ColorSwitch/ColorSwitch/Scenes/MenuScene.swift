0387e43c45fec8b23aa86c9cdcdf104545a1ef81
engineer-sbui <sbui.513@gmail.com>
2019-04-25 02:53:32 -0700

    add menu scene with logo and text

---
+++ GameDev/ColorSwitch/ColorSwitch/Scenes/MenuScene.swift
@@ -0,0 +1,55 @@
+//
+//  MenuScene.swift
+//  ColorSwitch
+//
+//  Created by Steven Bui on 4/25/19.
+//  Copyright © 2019 Steven Bui. All rights reserved.
+//
+
+import SpriteKit
+
+class MenuScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        backgroundColor = UIColor(red: 44/255, green: 62/255, blue: 80/255, alpha: 1.0)
+        
+        addLogo()
+        addLabels()
+    }
+    
+    func addLogo() {
+        let logo = SKSpriteNode(imageNamed: "logo")
+        logo.size = CGSize(width: frame.size.width/4, height: frame.size.width/4)
+        logo.position = CGPoint(x: frame.midX, y: frame.midY + frame.size.height/4)
+        addChild(logo)
+    }
+    
+    func addLabels() {
+        let playLabel = SKLabelNode(text: "Tape to Play!")
+        playLabel.fontName = "AvenirNext-Bold"
+        playLabel.fontSize = 50.0
+        playLabel.fontColor = UIColor.white
+        playLabel.position = CGPoint(x: frame.midX, y: frame.midY)
+        addChild(playLabel)
+        
+        let highScoreLabel = SKLabelNode(text: "High Score:")
+        highScoreLabel.fontName = "AvenirNext-Bold"
+        highScoreLabel.fontSize = 40.0
+        highScoreLabel.fontColor = UIColor.white
+        highScoreLabel.position = CGPoint(x: frame.midX, y: frame.midY - highScoreLabel.frame.size.height*4)
+        addChild(highScoreLabel)
+        
+        let recentScoreLabel = SKLabelNode(text: "Recent Score:")
+        recentScoreLabel.fontName = "AvenirNext-Bold"
+        recentScoreLabel.fontSize = 40.0
+        recentScoreLabel.fontColor = UIColor.white
+        recentScoreLabel.position = CGPoint(x: frame.midX, y: highScoreLabel.position.y - recentScoreLabel.frame.size.height*2)
+        addChild(recentScoreLabel)
+    
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let gameScene = GameScene(size: view!.bounds.size)
+        view!.presentScene(gameScene)
+    }
+}
