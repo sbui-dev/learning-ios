912f6255098389854e5a0561025d1f546ba0206f
engineer-sbui <sbui.513@gmail.com>
2019-04-25 02:23:44 -0700

    game logic

--- GameDev/ColorSwitch/ColorSwitch/Scenes/GameScene.swift
+++ GameDev/ColorSwitch/ColorSwitch/Scenes/GameScene.swift
@@ -8,9 +8,24 @@
 
 import SpriteKit
 
+enum PlayColors {
+    static let colors = [
+        UIColor(red: 231/255, green: 76/255, blue: 60/255, alpha: 1.0),
+        UIColor(red: 241/255, green: 196/255, blue: 15/255, alpha: 1.0),
+        UIColor(red: 46/255, green: 204/255, blue: 113/255, alpha: 1.0),
+        UIColor(red: 52/255, green: 152/255, blue: 219/255, alpha: 1.0)
+    ]
+}
+
+enum SwitchState : Int {
+    case red, yellow, green, blue
+}
+
 class GameScene: SKScene {
 
-    var colorCircle : SKSpriteNode!
+    var ColorSwitch : SKSpriteNode!
+    var switchState = SwitchState.red
+    var currentColorIndex : Int?
     
     override func didMove(to view: SKView) {
         setupPhysics()
@@ -25,22 +40,24 @@ class GameScene: SKScene {
     func layoutScene() {
         backgroundColor = UIColor(red: 44/255, green: 62/255, blue: 80/255, alpha: 1.0)
         
-        colorCircle = SKSpriteNode(imageNamed: "ColorCircle")
-        colorCircle.size = CGSize(width: frame.size.width/3, height: frame.size.width/3)
-        colorCircle.position = CGPoint(x: frame.midX, y: frame.minY + colorCircle.size.height)
-        colorCircle.physicsBody = SKPhysicsBody(circleOfRadius: colorCircle.size.width/2)
-        colorCircle.physicsBody?.categoryBitMask = PhysicsCategories.switchCategory
-        colorCircle.physicsBody?.isDynamic = false
+        ColorSwitch = SKSpriteNode(imageNamed: "ColorCircle")
+        ColorSwitch.size = CGSize(width: frame.size.width/3, height: frame.size.width/3)
+        ColorSwitch.position = CGPoint(x: frame.midX, y: frame.minY + ColorSwitch.size.height)
+        ColorSwitch.physicsBody = SKPhysicsBody(circleOfRadius: ColorSwitch.size.width/2)
+        ColorSwitch.physicsBody?.categoryBitMask = PhysicsCategories.switchCategory
+        ColorSwitch.physicsBody?.isDynamic = false
         
-        addChild(colorCircle)
+        addChild(ColorSwitch)
         
         spawnBall()
     }
     
     func spawnBall() {
-        let ball = SKSpriteNode(imageNamed: "ball")
+        currentColorIndex = Int(arc4random_uniform(UInt32(4)))
         
-        ball.size = CGSize(width: 30.0, height: 30.0)
+        let ball = SKSpriteNode(texture: SKTexture(imageNamed: "ball"), color: PlayColors.colors[currentColorIndex!], size: CGSize(width: 30.0, height: 30.0))
+        ball.colorBlendFactor = 1 // makes sure color is applied to texture
+        ball.name = "Ball"
         ball.position = CGPoint(x: frame.midX, y: frame.maxY)
         ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width/2)
         ball.physicsBody?.categoryBitMask = PhysicsCategories.ballCategory
@@ -48,6 +65,25 @@ class GameScene: SKScene {
         ball.physicsBody?.collisionBitMask = PhysicsCategories.none
         
         addChild(ball)
+    }
+    
+    func turnWheel() {
+        if let newState = SwitchState(rawValue: switchState.rawValue + 1) {
+            switchState = newState
+        }
+        else {
+            switchState = .red
+        }
+        
+        ColorSwitch.run(SKAction.rotate(byAngle: .pi/2, duration: 0.25))
+    }
+    
+    func gameOver() {
+        print("Game over")
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        turnWheel()
     }
 }
 
@@ -58,7 +94,19 @@ extension GameScene : SKPhysicsContactDelegate {
         let contactMask = contact.bodyA.categoryBitMask | contact.bodyB.categoryBitMask
         
         if contactMask == PhysicsCategories.ballCategory | PhysicsCategories.switchCategory {
-            print("contact occured")
+            if let ball = contact.bodyA.node?.name == "Ball" ? contact.bodyA.node as? SKSpriteNode : contact.bodyB.node as? SKSpriteNode {
+                if currentColorIndex == switchState.rawValue {
+                    print("Correct")
+                    
+                    ball.run(SKAction.fadeOut(withDuration: 0.25)) {
+                        ball.removeFromParent()
+                        self.spawnBall()
+                    }
+                }
+                else {
+                    gameOver()
+                }
+            }
         }
     }
 }
