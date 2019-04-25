dbb7a389310a45e5784f9f6c90424a58ab5071b5
engineer-sbui <sbui.513@gmail.com>
2019-04-25 03:04:50 -0700

    animate the tap to play label

--- GameDev/ColorSwitch/ColorSwitch/Scenes/MenuScene.swift
+++ GameDev/ColorSwitch/ColorSwitch/Scenes/MenuScene.swift
@@ -31,6 +31,7 @@ class MenuScene: SKScene {
         playLabel.fontColor = UIColor.white
         playLabel.position = CGPoint(x: frame.midX, y: frame.midY)
         addChild(playLabel)
+        animate(label: playLabel)
         
         let highScoreLabel = SKLabelNode(text: "High Score: " + "\(UserDefaults.standard.integer(forKey:"HighScore"))")
         highScoreLabel.fontName = "AvenirNext-Bold"
@@ -45,7 +46,17 @@ class MenuScene: SKScene {
         recentScoreLabel.fontColor = UIColor.white
         recentScoreLabel.position = CGPoint(x: frame.midX, y: highScoreLabel.position.y - recentScoreLabel.frame.size.height*2)
         addChild(recentScoreLabel)
+    }
     
+    func animate(label: SKLabelNode) {
+        //let fadeOut = SKAction.fadeOut(withDuration: 0.5)
+        //let fadeIn = SKAction.fadeIn(withDuration: 0.5)
+        
+        let scaleUp = SKAction.scale(to: 1.1, duration: 0.5)
+        let scaleDown = SKAction.scale(to: 1.0, duration: 0.5)
+        
+        let sequence = SKAction.sequence([scaleUp, scaleDown])
+        label.run(SKAction.repeatForever(sequence))
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
