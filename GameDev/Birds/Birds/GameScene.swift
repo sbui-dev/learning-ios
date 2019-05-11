778fcf173c1cab6cbd1ae23be82e5472010f3841
engineer-sbui <sbui.513@gmail.com>
2019-05-10 22:47:37 -0700

    add camera node and reorganize project

--- GameDev/Birds/Birds/GameScene.swift
+++ GameDev/Birds/Birds/GameScene.swift
@@ -11,8 +11,16 @@ import GameplayKit
 
 class GameScene: SKScene {
     
+    let gameCamera = SKCameraNode()
+    
     override func didMove(to view: SKView) {
-        
+        addCamera()
     }
    
+    func addCamera() {
+        guard let view = view else { return }
+        addChild(gameCamera)
+        gameCamera.position = CGPoint(x: view.bounds.size.width/2, y: view.bounds.size.height/2)
+        camera = gameCamera
+    }
 }
