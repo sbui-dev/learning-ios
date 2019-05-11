77e1fd9baab0e590d13474a1359cc87a94278bcb
engineer-sbui <sbui.513@gmail.com>
2019-05-10 22:58:15 -0700

    add panning functionality

---
+++ GameDev/Birds/Birds/GameCamera.swift
@@ -0,0 +1,13 @@
+//
+//  GameCamera.swift
+//  Birds
+//
+//  Created by Steven Bui on 5/10/19.
+//  Copyright © 2019 RUME Academy. All rights reserved.
+//
+
+import SpriteKit
+
+class GameCamera: SKCameraNode {
+
+}
--- GameDev/Birds/Birds/GameScene.swift
+++ GameDev/Birds/Birds/GameScene.swift
@@ -12,9 +12,17 @@ import GameplayKit
 class GameScene: SKScene {
     
     let gameCamera = SKCameraNode()
+    var panRecognizer = UIPanGestureRecognizer()
     
     override func didMove(to view: SKView) {
         addCamera()
+        setupGestureEcognizers()
+    }
+    
+    func setupGestureEcognizers() {
+        guard let view = view else { return }
+        panRecognizer = UIPanGestureRecognizer(target: self, action: #selector(pan))
+        view.addGestureRecognizer(panRecognizer)
     }
    
     func addCamera() {
@@ -22,5 +30,15 @@ class GameScene: SKScene {
         addChild(gameCamera)
         gameCamera.position = CGPoint(x: view.bounds.size.width/2, y: view.bounds.size.height/2)
         camera = gameCamera
+    }
+}
+
+extension GameScene {
+    
+    @objc func pan(sender: UIPanGestureRecognizer) {
+        guard let view = view else { return }
+        let translation = sender.translation(in: view)
+        gameCamera.position = CGPoint(x: gameCamera.position.x - translation.x, y: gameCamera.position.y + translation.y)
+        sender.setTranslation(CGPoint.zero, in: view)
     }
 }
