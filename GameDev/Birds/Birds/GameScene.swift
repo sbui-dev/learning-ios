b95973f385f2fd2d75bbb5fc11de6b8147305595
engineer-sbui <sbui.513@gmail.com>
2019-05-10 22:17:38 -0700

    initial skeleton project for birds

---
+++ GameDev/Birds/Birds/GameScene.swift
@@ -0,0 +1,18 @@
+//
+//  GameScene.swift
+//  Birds
+//
+//  Created by Johannes Ruof on 18.10.17.
+//  Copyright © 2017 RUME Academy. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+   
+}
---
+++ GameDev/Birds/Birds/GameViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GameViewController.swift
+//  Birds
+//
+//  Created by Johannes Ruof on 18.10.17.
+//  Copyright © 2017 RUME Academy. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+
+}
