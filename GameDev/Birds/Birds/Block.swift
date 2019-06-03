1577a8e497da88463c695f16aada1bf6c11b53f5
engineer-sbui <sbui.513@gmail.com>
2019-06-02 20:28:38 -0700

    initial implementation of block class

---
+++ GameDev/Birds/Birds/Block.swift
@@ -0,0 +1,38 @@
+//
+//  Block.swift
+//  Birds
+//
+//  Created by Steven Bui on 6/2/19.
+//  Copyright © 2019 RUME Academy. All rights reserved.
+//
+
+import SpriteKit
+
+enum BlockType: String {
+    case wood, stone, glass
+}
+
+class Block: SKSpriteNode {
+    
+    let type: BlockType
+    var health: Int
+    let damageThreshold: Int
+    
+    init(type: BlockType) {
+        self.type = type
+        switch type {
+        case .wood:
+            health = 200
+        case .stone:
+            health = 500
+        case .glass:
+            health = 50
+        }
+        damageThreshold = health / 2
+        super.init(texture: nil, color: UIColor.clear, size: CGSize.zero)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
